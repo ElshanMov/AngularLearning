c91c330404fc15889f46b484b4257226ce019988
ElshanMov <62793862+ElshanMov@users.noreply.github.com>
2022-11-28 00:28:26 +0400

    Admin and UI + Multiple Layout implemented

---
+++ src/app/admin/components/components.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrdersModule } from './orders/orders.module';
+import { CustomersModule } from './customers/customers.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { ProductsModule } from './products/products.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OrdersModule,
+    CustomersModule,
+    DashboardModule,
+    ProductsModule,
+  ],
+})
+export class ComponentsModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminModule } from './admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UiModule,
+    AdminModule,
+    RouterModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
